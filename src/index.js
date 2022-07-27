3f9aca3530b2a623db041c4c77bac61145523070
Victoria <victoria.belokon@gmail.com>
2022-07-27 03:25:05 +0200

    update js for date, time, wind

--- src/index.js
+++ src/index.js
@@ -1,70 +1,46 @@
-let now = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let h1 = document.querySelector("h1");
+  let day = days[date.getDay()];
 
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-h1.innerHTML = `${day} ${hours}:${minutes}`;
-
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temp-1").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  return `${day} ${hours}:${minutes}`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let apiKey = "742836e67df46810db3221e90416cefd";
-  let city = document.querySelector("#input").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  console.log(axios);
-  axios.get(apiUrl).then(displayWeatherCondition);
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+let apiKey = "742836e67df46810db3221e90416cefd";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-//////week-5//////
-
-function showPosition(position) {
-  alert(position.coords.latitude + position.coords.longitude);
-}
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-let button = document.querySelector("button");
-button.addEventListener("click", getCurrentPosition);
-
-/////////////////////////////////////////////////////////////////
-
-function showWeather(response) {
-  let html = document.querySelector("temp-1");
-  let temperature = Math.round(response.data.main.temp);
-  document.querySelector(
-    "temp-1"
-  ).innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
-}
-
-function retrievePosition(position) {
-  let apiKey = "742836e67df46810db3221e90416cefd";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(showWeather);
-}
-navigator.geolocation.getCurrentPosition(retrievePosition);
+axios.get(apiUrl).then(displayTemperature);
